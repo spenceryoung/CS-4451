6aabd72fe72a7923e2648dfc6d53caa67d7f5dd3
spenceryoung <syoung42788@gmail.com>
2016-06-16 17:12:18 -0500

    push

--- js/main.js
+++ js/main.js
@@ -2,23 +2,18 @@ var eValue = document.getElementById('encrypt-tbx'),
     dValue = document.getElementById('decrypt-tbx'),
     eBtn = document.getElementById('encrypt-btn'),
     dBtn = document.getElementById('decrypt-btn'),
+    eLbl = document.getElementById('encrypt-lbl'),
+    dLbl = document.getElementById('decrypt-lbl'),
     encrypted, decrypted;
 
 
 eBtn.addEventListener('click', function() {
     encrypted = CryptoJS.AES.encrypt(eValue.value, "Secret Passphrase");
     dValue.value = encrypted;
+    eLbl.textContent = encrypted;
 }, false);
 
-eBtn.addEventListener('click', function() {
-    //decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+dBtn.addEventListener('click', function() {
+    decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+    dLbl.textContent = decrypted.toString(CryptoJS.enc.Utf8);
 }, false);
-
-
-
-
-
-
-// document.getElementById("demo1").innerHTML = encrypted;
-// document.getElementById("demo2").innerHTML = decrypted;
-// document.getElementById("demo3").innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
