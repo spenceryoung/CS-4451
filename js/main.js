698dc27320e70f0c6da40825a973ff1177461ece
spenceryoung <syoung42788@gmail.com>
2016-07-03 00:50:26 -0500

    push

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-var eLbl = document.getElementById('encrypt-lbl'),
-    dLbl = document.getElementById('decrypt-lbl'),
-    encrypted, decrypted;
+var encrypted, decrypted;
 
 document.getElementById('encrypt-btn').addEventListener('click', function() {
     encrypted = CryptoJS.AES.encrypt(document.getElementById('encrypt-tbx').value,
         "Secret Passphrase");
-    eLbl.textContent = encrypted;
+    document.getElementById('encrypt-lbl').textContent = encrypted;
     document.getElementById('decrypt-tbx').value = encrypted;
 }, false);
 
 document.getElementById('decrypt-btn').addEventListener('click', function() {
     decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
-    dLbl.textContent = decrypted.toString(CryptoJS.enc.Utf8);
+    document.getElementById('decrypt-lbl').textContent = decrypted.toString(CryptoJS.enc.Utf8);
 }, false);
