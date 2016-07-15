9cc882163bb3b85df12be3d3234f740e26bd7147
spenceryoung <syoung42788@gmail.com>
2016-07-15 10:10:08 -0500

    push

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
-var encrypted, decrypted;
+var iInnput = document.getElementById('encrypt-tbx'),
+    dInnput = document.getElementById('decrypt-tbx'),
+    encrypted, decrypted;
 
 document.getElementById('encrypt-btn').addEventListener('click', function() {
-    encrypted = CryptoJS.AES.encrypt(document.getElementById('encrypt-tbx').value,
-        "Secret Passphrase");
-    document.getElementById('encrypt-lbl').textContent = encrypted;
-    document.getElementById('decrypt-tbx').value = encrypted;
+    if (iInnput.value == '') {
+        alert("Please enter a message");
+    } else {
+        encrypted = CryptoJS.AES.encrypt(iInnput.value, "Secret Passphrase");
+        document.getElementById('encrypt-lbl').textContent = encrypted;
+        dInnput.value = encrypted;
+    }
 }, false);
 
 document.getElementById('decrypt-btn').addEventListener('click', function() {
-    decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
-    document.getElementById('decrypt-lbl').textContent = decrypted.toString(CryptoJS.enc.Utf8);
+    if (dInnput.value == '') {
+        alert("Please enter a message");
+    } else {
+        decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+        document.getElementById('decrypt-lbl').textContent = decrypted.toString(CryptoJS.enc.Utf8);
+    }
 }, false);
